e9415ee0db8df5e547d5d3a2571600564b1e6b70
ItzBlueBerries <fruitsyberries@gmail.com>
2023-11-22 22:21:12 -0500

    Add project files.

---
+++ ShortcutEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShortcutLib.Shortcut;
+using SRML;
+using SRML.Utils.Enum;
+
+namespace ShortcutLib
+{
+    [EnumHolder]
+    internal class Enums
+    {
+        // public static readonly Identifiable.Id TEST_SLIME;
+    }
+
+    internal class ShortcutEntry : ModEntryPoint
+    {
+        public override void PreLoad() => HarmonyInstance.PatchAll();
+
+        public override void Load()
+        {
+
+        }
+
+        public override void PostLoad()
+        {
+            
+        }
+    }
+}
