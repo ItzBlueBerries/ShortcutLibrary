731ea964853609d9969948eb4b1f6ede0a4c093f
ItzBlueBerries <fruitsyberries@gmail.com>
2023-11-29 20:07:44 -0500

    Ornament, Registry.

--- Shortcut/Ornament.cs
+++ Shortcut/Ornament.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using ShortcutLib.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,23 @@ using System.Threading.Tasks;
 
 namespace ShortcutLib.Shortcut
 {
+    /// <summary>
+    /// Method(s) for creating ornaments.
+    /// </summary>
     public static class Ornament
     {
-        public static GameObject CreateOrnament(Identifiable.Id baseIdentifiable, Identifiable.Id identifiable, Sprite icon, string name, Texture2D texture, Color32 color, Color32 vacColor)
+        /// <summary>
+        /// Creates a starting base for a custom ornament.
+        /// </summary>
+        /// <param name="baseIdentifiable">The base <see cref="Identifiable.Id"/> that the ornament copies the prefab <see cref="GameObject"/> from.</param>
+        /// <param name="identifiable">The <see cref="Identifiable.Id"/> of the ornament.</param>
+        /// <param name="icon">The icon <see cref="Sprite"/> of the ornament.</param>
+        /// <param name="name">The name <see cref="string"/> of the ornament.</param>
+        /// <param name="texture">The texture <see cref="Texture2D"/> of the ornament.</param>
+        /// <param name="color">The color <see cref="Color"/> of the ornament.</param>
+        /// <param name="vacColor">The vac color <see cref="Color"/> of the ornament.</param>
+        /// <returns><see cref="GameObject"/></returns>
+        public static GameObject CreateOrnamentBase(Identifiable.Id baseIdentifiable, Identifiable.Id identifiable, Sprite icon, string name, Texture2D texture, Color32 color, Color32 vacColor)
         {
             GameObject prefab = Prefab.ObjectCopy(Prefab.GetPrefab(baseIdentifiable));
             prefab.name = "ornament" + name.Replace(" ", "");
@@ -22,10 +37,12 @@ namespace ShortcutLib.Shortcut
             material.color = color;
             model.GetComponent<MeshRenderer>().material = material;
 
-            /*Registry.RegisterIdentPrefab(OrnamentPrefab);
-            Registry.RegisterAmmo(OrnamentPrefab);
-            Registry.RegisterSilo(ornamentIdent);
-            Registry.RegisterVac(vacColor, ornamentIdent, ornamentIcon, ornamentName.ToLower().Replace(" ", "") + "Definition");*/
+            Registry.AddIdentifiableToAmmo(identifiable);
+            Registry.AddIdentifiableToSilo(identifiable);
+            Registry.RegisterVaccable(identifiable, icon, vacColor, name.Replace(" ", "") + "Ornament");
+            Translate.Actor("l." + identifiable.ToString().ToLower(), name);
+
+            LookupRegistry.RegisterIdentifiablePrefab(prefab);
             return prefab;
         }
     }
